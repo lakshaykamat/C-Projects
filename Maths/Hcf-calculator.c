c4dd3fac45b9fb6626f6d97453bd23323dd35b63
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-01-28 18:01:37 +0530

    New files added

--- Maths/Hcf-calculator.c
+++ Maths/Hcf-calculator.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-int main() {
-    int num,num2,hcf,u;
-    label:printf("Enter number1 = ");
-    scanf("%d %d",&num,&num2);
-    
-    for(int i=1;i<=num && i<= num2 ;i++){
-        if( (num%i==0) && (num2%i==0) ){
-            hcf=i;
+int main()
+{
+    int num, num2, hcf;
+    printf("Enter number1 = ");
+    scanf("%d", &num);
+    printf("Enter number2 = ");
+    scanf("%d", &num2);
+
+    for (int i = 1; i <= num && i <= num2; i++)
+    {
+        if ((num % i == 0) && (num2 % i == 0))
+        {
+            hcf = i;
         }
     }
-        printf("Answer is %d\n",hcf);
-        printf("O to retry 1 to exit = ");
-        scanf("%d",&u);
-    if(u==1){
-        goto label;
-        
-    }
+    printf("HCF of %d and %d is %d\n",num,num2,hcf);
 
     return 0;
 }
