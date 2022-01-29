39e5248cb8295fb2cbab66f4fe41221e9cf126bd
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-01-29 08:27:35 +0530

    upddated

--- Others/height.c
+++ Others/height.c
@@ -1,4 +1,4 @@
-#include <std>
+#include <stdio.h>
 
 int main()
 {
@@ -21,58 +21,110 @@ int main()
         printf("Enter Height = ");
         scanf("%lf", &height);
         double cm = height / 10.0;
-        double m =  height/1000.0;
-        double km =  height/1e+6;
-        double in =  height/25.4;
-        double feet =  height/305.0;
-        double yard =  height/914.0;
-        double miles =  height/914.0;
-        double nautical-miles =  height/1.852e+6;
+        double m = height / 1000.0;
+        double km = height / 1e+6;
+        double in = height / 25.4;
+        double feet = height / 305.0;
+        double yard = height / 914.0;
+        double miles = height / 914.0;
+        double nautical_miles = height / 1.852e+6;
 
         printf("In %.1lfmm is = %.2lfcm\n", height, cm);
         printf("In %.1lfmm is = %.2lfkm\n", height, km);
         printf("In %.1lfmm is = %.2lfm\n", height, m);
         printf("In %.1lfmm is = %.2lfin\n", height, in);
+        printf("In %.1lfmm is = %.2lfft\n", height, feet);
+        printf("In %.1lfmm is = %.2lfyd\n", height, yard);
+        printf("In %.1lfmm is = %.2lfmil\n", height, miles);
+        printf("In %.1lfmm is = %.2lfNM\n", height, nautical_miles);
     }
     else if (input == 2)
     {
+        double mm = height * 10;
+        double m = height / 100.0;
+        double km = height / 100000.0;
+        double in = height / 25.4;
+        double feet = height / 30.48;
+        double yard = height / 91.44;
+        double miles = height / 160934.0;
+        double nautical_miles = height / 185200.0;
         printf("Enter Height = ");
         scanf("%lf", &height);
-        double mi_h = height / 1.609, km = height / 1.097, m = height / 3.6, in = height / 1.852;
-        printf("In %.1lfcm is = %.2lfcm\n", height, mi_h);
+
+        printf("In %.1lfcm is = %.2lfmm\n", height, mm);
+        printf("In %.1lfcm is = %.2lfm\n", height, m);
         printf("In %.1lfcm is = %.2lfkm\n", height, km);
-        printf("In %.1lfcm is = %.2lkm\n", height, m);
-        printf("In %.1lfcm is = %.2lfkn\n", height, in);
+        printf("In %.1lfcm is = %.2lfin\n", height, in);
+        printf("In %.1lfcm is = %.2lfft\n", height, feet);
+        printf("In %.1lfcm is = %.2lfyd\n", height, yard);
+        printf("In %.1lfcm is = %.2lfmil\n", height, miles);
+        printf("In %.1lfcm is = %.2lfNM\n", height, nautical_miles);
     }
     else if (input == 3)
     {
+        double mm = height * 1000.0;
+        double cm = height * 100.0;
+        double km = height / 1000.0;
+        double in = height / 39.37;
+        double feet = height / 3.281;
+        double yard = height / 1.094;
+        double miles = height / 1609.0;
+        double nautical_miles = height / 1852.0;
         printf("Enter Height = ");
         scanf("%lf", &height);
-        double mi_h = height / 1.467, m = height / 3.281, in = height / 1.6888, cm = height * 1.097;
-        printf("In %.1lfkm is = %.2lfcm\n", height, mi_h);
-        printf("In %.1lfkm is = %.2lkm\n", height, m);
-        printf("In %.1lfkm is = %.2lfcm\n", height, cm);
-        printf("In %.1lfkm is = %.2lfkn\n", height, in);
+
+        printf("In %.1lfcm is = %.2lfmm\n", height, mm);
+        printf("In %.1lfcm is = %.2lfcm\n", height, cm);
+        printf("In %.1lfcm is = %.2lfkm\n", height, km);
+        printf("In %.1lfcm is = %.2lfin\n", height, in);
+        printf("In %.1lfcm is = %.2lfft\n", height, feet);
+        printf("In %.1lfcm is = %.2lfyd\n", height, yard);
+        printf("In %.1lfcm is = %.2lfmil\n", height, miles);
+        printf("In %.1lfcm is = %.2lfNM\n", height, nautical_miles);
     }
     else if (input == 4)
     {
+        double mm = height * 1e+6;
+        double cm = height * 100000.0;
+        double m = height * 1000.0;
+        double in = height / 39370.0;
+        double feet = height / 3281.0;
+        double yard = height / 1094.0;
+        double miles = height / 1609.0;
+        double nautical_miles = height / 1.852;
         printf("Enter Height = ");
         scanf("%lf", &height);
-        double mi_h = height * 2.237, ft_s = height * 3.281, in = height * 1.944, cm = height * 3.6;
-        printf("In %.1lkm is = %.2lfcm\n", height, mi_h);
-        printf("In %.1lkm is = %.2lfkm\n", height, ft_s);
-        printf("In %.1lkm is = %.2lfcm\n", height, cm);
-        printf("In %.1lkm is = %.2lfkn\n", height, in);
+
+        printf("In %.1lfcm is = %.2lfmm\n", height, mm);
+        printf("In %.1lfcm is = %.2lfcm\n", height, cm);
+        printf("In %.1lfcm is = %.2lfkm\n", height, m);
+        printf("In %.1lfcm is = %.2lfin\n", height, in);
+        printf("In %.1lfcm is = %.2lfft\n", height, feet);
+        printf("In %.1lfcm is = %.2lfyd\n", height, yard);
+        printf("In %.1lfcm is = %.2lfmil\n", height, miles);
+        printf("In %.1lfcm is = %.2lfNM\n", height, nautical_miles);
     }
     else if (input == 5)
     {
+        double mm = height * 305.0;
+        double cm = height * 30.48;
+        double m = height * 3.281;
+        double in = height * 12.0;
+        double km = height / 3281.0;
+        double yard = height / 3.0;
+        double miles = height / 5280.0;
+        double nautical_miles = height / 6076.0;
         printf("Enter Height = ");
         scanf("%lf", &height);
-        double mi_h = height * 1.15077, ft_s = height * 1.68780, cm = height * 1.852, m = height * 0.5144;
-        printf("In %.1lfkn is = %.2lfcm\n", height, mi_h);
-        printf("In %.1lfkn is = %.2lfkm\n", height, ft_s);
-        printf("In %.1lfkn is = %.2lfcm\n", height, cm);
-        printf("In %.1lfkn is = %.2lfkn\n", height, m);
+
+        printf("In %.1lfcm is = %.2lfmm\n", height, mm);
+        printf("In %.1lfcm is = %.2lfcm\n", height, cm);
+        printf("In %.1lfcm is = %.2lfkm\n", height, m);
+        printf("In %.1lfcm is = %.2lfin\n", height, in);
+        printf("In %.1lfcm is = %.2lfft\n", height, km);
+        printf("In %.1lfcm is = %.2lfyd\n", height, yard);
+        printf("In %.1lfcm is = %.2lfmil\n", height, miles);
+        printf("In %.1lfcm is = %.2lfNM\n", height, nautical_miles);
     }
     return 0;
 }
