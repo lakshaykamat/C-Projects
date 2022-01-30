a36f9424b50b47bda5d034d59387203bd2a243e5
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-01-30 16:22:40 +0530

    math.h function added

--- Maths/power_number.c
+++ Maths/power_number.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-int main() {
+#include <math.h>
+int main()
+{
 
     /*
-    Enter a Number and Power 
+    Enter a Number and Power
     */
-    int input,pow,sum= 1;;
-    scanf("%d",&input);
-    scanf("%d",&pow);
-    
-    for(int i = 1 ;i<=pow;i++){
-        sum = sum * input;
-    }
-    printf("%d to the power %d is = %d",input,pow,sum);
+    int input, powe, sum = 1;
+    printf("Enter num = ");
+    scanf("%d", &input);
+    printf("Enter power = ");
+    scanf("%d", &powe);
+
+    printf("%d to the power %d is = %.0lf", input, powe, pow(input, powe));
+    //Math.h function added  of power NOTE pow function data type is double %.0lf
+
+
+    //Alternate Type
+    // for (int i = 1; i <= pow; i++)
+    // {
+    //     sum = sum * input;
+    //     
+    // }
+    // printf("%d to the power %d is = %d", input, pow, sum);
     return 0;
 }
