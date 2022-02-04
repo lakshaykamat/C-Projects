ad2c46b38a5a88a79bd0ccd0c4714c7932b24f97
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-02-04 06:53:25 +0530

    fibonacci added

---
+++ Others/fibonacci-series-fumctions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibo(int num)
+{
+    int n0 = 0, n1 = 1, a;
+    /*fibo 0 1 1 2 3 5 8...*/
+    for (int i = 3; i <= num; i++)
+    {
+        a = n0 + n1; // 0 + 1 = 1 // 1 + 1 = 2
+        n0 = n1;     // n0 = 1 // 1  = 1 
+        n1 = a;      // 1 = 1
+       printf("%d ", a);
+        // return a;
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter the Num = ");
+    scanf("%d",&num);
+    int b = fibo(num);
+    return 0;
+}
