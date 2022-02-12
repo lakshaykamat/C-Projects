726bc87ce26a5c282f88bf789986b71df3232c5f
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-02-12 21:29:02 +0530

    updated

--- Health/Bmi-calculator.c
+++ Health/Bmi-calculator.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+    char ch;
     double kg, cm, bmi, m;
     printf("If you don't Know in kg then 1lbs = 0.45kg: \n");
     printf("If you don't Know in cm then 1cm = 0.01m: \n");
 
-    printf("Enter you kg in Kilogram(kg): ");
+    label:
+    printf("Enter you weight in Kilogram(kg): ");
     scanf("%lf", &kg);
 
-    printf("Enter you Height in Centimetere: ");
+    printf("Enter you Height in Centimetere(cm): ");
     scanf("%lf", &cm);
     printf("\n\n");
 
@@ -22,11 +24,13 @@ int main(int argc, char const *argv[])
         printf("Eat more and Don't Masterbate 😋\n");
 
         double needweight = 18.5 * (m * m);
-        printf("You Need Minimum %.2lf kg weight ", needweight);
+        double gain = needweight - kg;
+        printf("You Need to Gain %.3lfkg weight\n",gain);
+        printf("You Need Minimum %.2lf kg weight\n\n", needweight);
     }
     else if ((bmi >= 18.5) && (bmi <= 24.9))
     {
-        printf("All Good! You are Normal You can do whatever you want 😎\n");
+        printf("All Good! You are Normal You can do whatever you want 😎\n\n");
     }
     else if ((bmi >= 25.0) && (bmi <= 29.9))
     {
@@ -35,7 +39,7 @@ int main(int argc, char const *argv[])
         double needweight = 24  * (m * m);
         double lose = kg - needweight;
         printf("You Need lose %.2lfkg weight\n", lose);
-        printf("Your body weight requriement %.2lfkg weight\n", needweight);
+        printf("Your body weight requriement %.2lfkg weight\n\n", needweight);
     }
     else if ((bmi >= 30.0))
     {
@@ -43,8 +47,15 @@ int main(int argc, char const *argv[])
         printf("Do Some Hardwork in Life purse a gym 😟	 \n\n");
         double needweight = 24  * (m * m);
         double lose = kg - needweight;
-        printf("You Need lose %.2lfkg weight ", lose);
-        printf("Your body weight requriement %.2lfkg weight ", needweight);
+        printf("You Need lose %.2lfkg weight\n", lose);
+        printf("Your body weight requriement %.2lfkg weight\n\n", needweight);
+    }
+
+    printf("Do you want to Run Again (y/n)?");
+    scanf(" %c",&ch);
+    if (ch == 'y' || ch == 'Y')
+    {
+    goto label;
     }
     return 0;
 }
