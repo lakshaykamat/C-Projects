93951aa50768ce48c029e136f00555dadbe1786e
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-02-13 12:20:08 +0530

    modified

--- Health/Bmi-calculator.c
+++ Health/Bmi-calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <windows.h>
 int main(int argc, char const *argv[])
 {
     char ch;
@@ -6,7 +7,7 @@ int main(int argc, char const *argv[])
     printf("If you don't Know in kg then 1lbs = 0.45kg: \n");
     printf("If you don't Know in cm then 1cm = 0.01m: \n");
 
-    label:
+label:
     printf("Enter you weight in Kilogram(kg): ");
     scanf("%lf", &kg);
 
@@ -16,16 +17,16 @@ int main(int argc, char const *argv[])
 
     m = cm / 100;
     bmi = (kg / (m * m));
-    printf("Your Body Mass Index (BMI) is %.1lf\n", bmi);
+    printf("Your Body Mass Index (BMI) is %.1lf\n\n", bmi);
 
     if (bmi <= 18.5)
     {
         printf("Hey! You are Underweight\n");
-        printf("Eat more and Don't Masterbate 😋\n");
+        printf("Eat more and Don't Masterbate 😋\n\n");
 
         double needweight = 18.5 * (m * m);
         double gain = needweight - kg;
-        printf("You Need to Gain %.3lfkg weight\n",gain);
+        printf("You Need to Gain %.3lfkg weight\n", gain);
         printf("You Need Minimum %.2lf kg weight\n\n", needweight);
     }
     else if ((bmi >= 18.5) && (bmi <= 24.9))
@@ -35,8 +36,8 @@ int main(int argc, char const *argv[])
     else if ((bmi >= 25.0) && (bmi <= 29.9))
     {
         printf("Hey! Look at you you are overweight nigga\n");
-        printf("Join a Gym now if not no one will marry you 🤨\n");
-        double needweight = 24  * (m * m);
+        printf("Join a Gym now if not no one will marry you 🤨\n\n");
+        double needweight = 24 * (m * m);
         double lose = kg - needweight;
         printf("You Need lose %.2lfkg weight\n", lose);
         printf("Your body weight requriement %.2lfkg weight\n\n", needweight);
@@ -45,17 +46,27 @@ int main(int argc, char const *argv[])
     {
         printf("Hey! Why you are here... Look yourself\n");
         printf("Do Some Hardwork in Life purse a gym 😟	 \n\n");
-        double needweight = 24  * (m * m);
+        double needweight = 24 * (m * m);
         double lose = kg - needweight;
         printf("You Need lose %.2lfkg weight\n", lose);
         printf("Your body weight requriement %.2lfkg weight\n\n", needweight);
     }
-
+above:
     printf("Do you want to Run Again (y/n)?");
-    scanf(" %c",&ch);
+    scanf(" %c", &ch);
     if (ch == 'y' || ch == 'Y')
     {
-    goto label;
+        goto label;
+    }
+    else if (ch == 'n' || ch == 'N')
+    {
+        printf("Program is End!");
+    }
+    else
+    {
+        Beep(550, 500);
+        printf("Invalid Input!\n Try Again\n\n");
+        goto above;
     }
     return 0;
 }
--- Pointer/call-by-reference.c
+++ Pointer/call-by-reference.c
@@ -8,7 +8,7 @@ int main()
 {
     // Here both actual and formal parameters refer to same memory location. Therefore, any changes made to the formal parameter will get reflected to actual parameters.
     int x = 10, y = 20;
-    printf("x = %d y = %d\n", x, y);
+    printf("x z= %d y = %d\n", x, y);
     function(&x, &y);
     printf("x = %d y = %d\n", x, y);
     return 0;
